07e39e4657d22803a1ee82cbe9ba66e1ad81304e
OkkyHaryanto <63486943+OkkyHaryanto@users.noreply.github.com>
2022-01-13 12:41:27 +0700

    Progress

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ function signOut() {
         $(".g-signin2").css("display", "block");
     });
 }
-
+$(document).ready(function() {
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#dataTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+  });
 $("#dataTable").ready(function () {
     var tabel = document.getElementById("dataTable")
     getAll().then(response => {
